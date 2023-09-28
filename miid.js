a6bf019034d3b12e9f810a06d5f9144848afc08d
Am-miid <127655149+Am-miid@users.noreply.github.com>
2023-09-28 22:47:12 +0100

    Add files via upload

---
+++ miid.js
@@ -0,0 +1,35 @@
+// const form = document.getElementById("form");
+// const psswrd = document.getElementById(psswrd);
+// const cnfrmpsswrd = document.getElementById(cnfrmpsswrd);
+// const messageContainer = document.querySelector("message_container");
+// const message = document.getElementById("message");
+// let isValid = false;
+
+// // validateForm
+// //   
+// // passwordMatch condition
+// //         var  psswrd = cnfrmpsswrd;
+// //     if( psswrd.value.match === cnfrmpsswrd.value.match) {
+// //         passwordMatch = true;
+// //         psswrd.style.borderColor = "green";
+// //         cnfrmpsswrd.style.borderColor = "green";
+// //     } else{
+// //         passwordMatch = false;
+// //         psswrd.style.borderColor = "red";
+// //         cnfrmpsswrd.style.borderColor = "red";
+// //     }
+// // }
+
+// // storeUser information
+// function storeUser() {
+
+// }
+
+
+// // processFormdata   
+// function procssFormData(event) {
+//     event.preventDefault();
+//     // validateForm(); 
+// }
+
+// form.addEventListener("submit",procssFormData);
